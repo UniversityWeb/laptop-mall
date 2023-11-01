7d7841a4911cdc80282daf635b0a965677278714
AnhAnNek <info.vananne@gmail.com>
2023-11-01 16:20:07 +0700

    add displayName field for EGender

--- src/main/java/com/webteam/laptopmall/customenum/EGender.java
+++ src/main/java/com/webteam/laptopmall/customenum/EGender.java
@@ -1,5 +1,16 @@
 package com.webteam.laptopmall.customenum;
 
 public enum EGender {
-    MALE, FEMALE
+    MALE("Male"),
+    FEMALE("Female");
+
+    private final String displayName;
+
+    EGender(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
